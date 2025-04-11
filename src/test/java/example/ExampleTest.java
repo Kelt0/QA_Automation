11489c31c95896ea982439ad5672dbf37778acf8
Mikhail Polyakov <varden.polyakov@yandex.ru>
2025-04-11 18:49:30 +0300

    Some additions

---
+++ src/test/java/example/ExampleTest.java
@@ -0,0 +1,11 @@
+package example;
+
+import org.testng.annotations.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void sampleTest() {
+        System.out.println("Test executed");
+    }
+}
